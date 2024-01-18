8f915d083b5ac8001e8fc5efad9a47559c916068
Nirzon Karmakar <nirzon47@gmail.com>
2024-01-18 17:08:06 +0530

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Header from './Components/Header'
+
 const App = () => {
-	return <div>App</div>
+	return (
+		<>
+			<Header />
+		</>
+	)
 }
 
 export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className='bg-base-300 p-4'>
+			<h2 className='text-2xl text-center font-bold'>Directory App</h2>
+		</header>
+	)
+}
+
+export default Header
